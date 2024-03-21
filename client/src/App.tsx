12549f68ba7d248901fcf7d4d293d142205ccbf6
Thomas Nguyen <tomxnguy@gmail.com>
2024-03-21 20:14:18 +0000

    Added homepage to app.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 export default function App() {
@@ -22,12 +20,6 @@ export default function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <p>Initial Commit</p>
       </div>
       <h1>{serverData}</h1>
